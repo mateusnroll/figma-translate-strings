de90632ffa85e21a16c9e539248786581c747e81
Mateus Pinheiro <mateus.pinheiro@gympass.com>
2025-04-05 11:46:44 -0300

    UI suports Update and Localize actions

--- code.ts
+++ code.ts
@@ -1,5 +1,5 @@
 // Shows the HTML page in "ui.html"
-figma.showUI(__html__);
+figma.showUI(__html__, { width: 400, height: 800, title: "Lokalise translator" });
 
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
